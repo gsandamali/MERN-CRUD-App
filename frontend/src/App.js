dd5cb4085f5e72e9f939fe99c3585d1167859a41
gsandamali <gsandamali30@gmail.com>
2022-01-28 12:13:16 +0530

    Create Navigation Bar Using Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,46 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes, Route, Link} from 'react-router-dom';
 
-class App extends Component{
+// import component
+import Create from './components/create.component';
+import Edit from './components/edit.component';
+import Index from './components/index.component';
 
-  render() {
+class App extends Component {
+  render () {
     return(
-      <div className='container'>
-        <h2>CRUD App</h2>
+      <div>
+        <div className='container'>
+          <nav className='navbar navbar-expand-lg navbar-light bg-light mx-auto'>
+            <Link to={'/'} className='navbar-brand'> MERN CRUD</Link>
+            <div className='collapse navbar-collapse' id='navbarContent'>
+              <ul className='navbar-nav mr-auto'>
+                <li className="nav-item">
+                  <Link to={'/'} className="nav-link navbar-brand">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/create'} className="nav-link">Create</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/edit'} className="nav-link">Edit</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/index'} className="nav-link">Index</Link>
+                </li>
+              </ul>
+            </div>
+          </nav> <br/>
+          <h2>MERN CRUD</h2> <br/>
+          <Routes>
+            <Route exact path = '/create' component = {Create} />
+            <Route exact path = '/edit/:id' component = {Edit} />
+            <Route exact path = '/index' component = {Index} />
+          </Routes>
+        </div>
       </div>
     );
-  }
-
-}
+  };
+};
 
 export default App;
