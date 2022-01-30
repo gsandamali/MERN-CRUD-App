e8459fd67718e63479aa07c5fb7ed84f70127e13
gsandamali <gsandamali30@gmail.com>
2022-01-30 10:40:18 +0530

    Create functions to catch the changing state of user details

--- frontend/src/components/create.component.js
+++ frontend/src/components/create.component.js
@@ -1,6 +1,75 @@
 import React, {Component} from 'react';
 
 export default class Create extends Component{
+
+  //Create constructor to pass values
+  constructor(props){
+    super(props);
+
+    // catch the status changes of user details
+    this.onChangeUserID = this.onChangeUserID.bind(this);
+    this.onChangeFirstName = this.onChangeFirstName.bind(this);
+    this.onChangeLastName = this.onChangeLastName.bind(this);
+    this.onChangeContactNumber = this.onChangeContactNumber.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePosition = this.onChangePosition.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    //set initial state
+    this.state = {
+      user_id:"",
+      first_name:"",
+      last_name:"",
+      contact_number:"",
+      email:"",
+      position:""
+    }
+  }
+
+  //set the state with target value
+  onChangeUserID(e){
+    this.setState({
+      user_id:e.target.value
+    });
+  }
+  onChangeFirstName(e){
+    this.setState({
+      first_name:e.target.value
+    });
+  }
+  onChangeLastName(e){
+    this.setState({
+      last_name:e.target.value
+    });
+  }
+  onChangeContactNumber(e){
+    this.setState({
+      contact_number:e.target.value
+    });
+  }
+  onChangeEmail(e){
+    this.setState({
+      email:e.target.value
+    });
+  }
+  onChangePosition(e){
+    this.setState({
+      position:e.target.value
+    });
+  }
+  onSubmit(e){
+    e.preventDefault();
+    console.log(`The values are ${this.state.user_id}`);
+    this.setState({
+      user_id:"",
+      first_name:"",
+      last_name:"",
+      contact_number:"",
+      email:"",
+      position:""
+    })
+  }
+
   render() {
     return(
       <div style={{marginTop: 10}}>
