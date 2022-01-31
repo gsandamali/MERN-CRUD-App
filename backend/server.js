4aad60a38d74018a8d784254e9b6f848f7c0b814
gsandamali <gsandamali30@gmail.com>
2022-01-31 09:28:46 +0530

    connect the mondoDB to server

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,16 @@ const bodyParser = require('body-parser');
 const PORT = 4000;
 const cors = require('cors');
 
+const mongoose = require('mongoose');
+const config = require('./DB');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, {
+    useNewUrlParser: true
+})
+.then( function onFulfilled () { console.log(`MongoDB connected...`) } )
+.catch((err) => console.log('cannot connect to the databased' + err));
+
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 
