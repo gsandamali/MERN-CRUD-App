cdc4485966b049560ead47e654093e26014f56a0
gsandamali <gsandamali30@gmail.com>
2022-01-31 12:37:40 +0530

    add and use users route file

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 
 const mongoose = require('mongoose');
 const config = require('./DB');
+const userRoute = require('./user.routes');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, {
@@ -16,6 +17,8 @@ mongoose.connect(config.DB, {
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.use('/users', userRoute);
 
 app.listen(PORT, function(){
     console.log(`Server is running on port:`, PORT);
